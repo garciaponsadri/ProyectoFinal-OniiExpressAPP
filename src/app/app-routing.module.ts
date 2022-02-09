6f882c123f88cc1f5ca7a573b1f3dd769f3e6b3d
Adri <91051059+garciaponsadri@users.noreply.github.com>
2022-02-09 09:17:03 +0100

    Agregado Bootstrap y primer componente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmpleadosListComponent } from './components/empleados-list/empleados-list.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+ // {path: '/listar', component: EmpleadosListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmpleadosListComponent } from './components/empleados-list/empleados-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmpleadosListComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
